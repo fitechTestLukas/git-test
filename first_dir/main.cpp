d6271d1638c430ef77e3938c29f7ad128a7c8c94
fitechTestLukas <lukasz1081@wp.pl>
2021-07-15 13:50:12 +0200

    again change in first_dir

--- first_dir/main.cpp
+++ first_dir/main.cpp
@@ -9,7 +9,7 @@ int main() {
   for (int i = 1; i <= 100; i++){
     if (i % 3 == 0 && i % 5 == 0){
       finalAnswer = "FizzBuzz";
-      std::cout << finalAnswer << std::endl;
+      std::cout << finalAnswer+ "sadsads" << std::endl;
     }
     else if (i % 3 == 0){
       finalAnswer = "Fizz";
